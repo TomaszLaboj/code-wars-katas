55d7c95e38d6d1caae37a706230cf0f621d5e577
TomaszLaboj <tomasz.laboj@gmail.com>
2023-10-08 17:00:46 +0100

    more test for neutralise

--- src/8kyu/neutralise.test.ts
+++ src/8kyu/neutralise.test.ts
@@ -3,3 +3,6 @@ import { neutralise } from "./neutralise";
 test("should return all zeros", () => {
     expect(neutralise("+++", "---")).toEqual("000");
 });
+test("should return -0-", () => {
+    expect(neutralise("-+-", "---")).toEqual("-0-");
+});
